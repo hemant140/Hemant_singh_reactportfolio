52cf82630bd62d0b788639da5c781a84c577024e
Hemant <hemo.hm576@gmail.com>
2023-04-17 16:10:23 +0530

    portfolio update

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -91,14 +91,14 @@ const Skills = () => {
                 </div>
 
                 <div className='w-full grid
-                 grid-cols-2 sm:grid-cols-4 gap-8 text-center  px-12 sm:px-0'>
+                 grid-cols-2 sm:grid-cols-4 gap-8 text-center group px-12 sm:px-0'>
 
 
                     {tech.map(({ id, src, title, style }) => (
 
                         <div
                             key={id}
-                            className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}>
+                            className={`shadow-md group-hover:scale-110 duration-300 py-2 rounded-lg ${style}`}>
                             <img src={src} alt="html" className='w-20 mx-auto' />
                             <p className='mt-4'>{title}</p>
                         </div>
