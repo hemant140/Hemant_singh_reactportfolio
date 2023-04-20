4555e84a458094e48c5b3902fafd36fcf8d994ba
Hemant <hemo.hm576@gmail.com>
2023-04-20 16:08:40 +0530

    about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -18,12 +18,9 @@ const About = () => {
                     <div className='flex-1'>
                         <p className=' mx-2 text-5xl max-lg:text-3xl max-md:text-4xl font-bold inline border-b-4 border-gray-500 max-[390px]:text-3xl max-md:mx-0'>About</p>
                         <p className='text-lg  px-2 max mt-10 text-gray-600 text-justify max-md:text-lg max-[390px]:text-base' >
-                            <span className='text-lg text-gray-600 max-lg:text-base max-md:text-lg max-[390px]:text-base'>Hi,</span><br /> I am Hemant Singh, a graduate in Computer Science and Engineering from Veer Bahadur Singh Purvanchal University in Jaunpur, with a 72% grade.
-
-                            Skilled in C++, Python, Flutter, HTML, CSS, JavaScript, Jquery, Reactjs, MySQL, etc. Created minor and major projects, including a malware detection project using machine learning and a notification app built with Flutter, Dart, and Firebase.
-
-                            Currently learning Reactjs and working on web development projects after gaining proficiency in HTML, CSS, JavaScript, Jquery, and Bootstrap.
-                            <br /> <span className='text-xl text-gray-500 max-lg:text-lg max-md:text-xl max-[390px]:text-lg'> Thanks You!</span>
+                            <span className='text-lg text-gray-600 max-lg:text-lg max-md:text-lg max-[390px]:text-base'>Hi,</span><br /> 
+                           I'm  Hemant Singh ,a graduate Computer Science and Engineering from Veer Bahadur Singh Purvanchal University, Jaunpur, with a academic record of 72%. Proficient in various programming languages including C++, Python, and Flutter, as well as web development technologies such as HTML, CSS, JavaScript, Jquery, and Reactjs. Has experience in creating minor and major projects, including a machine learning-based malware detection project and a notification app built with Flutter, Dart, and Firebase. Currently focused on enhancing skills in Reactjs and working on web development projects using HTML, CSS, JavaScript, Jquery, and Bootstrap. With a passion for technology and a commitment to excellence,I'm ready to contribute to the success of any organization in the field of computer science and engineering.
+                            <br /> <span className='text-lg text-gray-500 max-lg:text-lg max-md:text-lg max-[390px]:text-lg'> Thanks You!</span>
                         </p>
                         <div>
                             <a href="Hemant_Singh_Resume.pdf" target='_blank' ><button className="text-white font-bold bg-teal-500 w-fit px-4 py-2 my-2 rounded-md max-sm:px-3 max-sm:py-2 hover:scale-110 duration-300 cursor-pointer">
